af5168c5f72e029eae44697888aa4248ecd39f12
Shahab nf <shahab.nikfetrat@gmail.com>
2022-08-07 23:18:26 -0400

    PR4

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ DB_Database = os.environ.get('DB_Database') or "mysql"
 DB_User = os.environ.get('DB_User')
 DB_Password = os.environ.get('DB_Password')
 group_name = os.environ.get('GROUP_NAME')
+fail_url = os.environ.get('FAIL_URL') 
+success_url = os.environ.get('SUCCESS_URL')
 
 @app.route("/")
 def main():
@@ -24,7 +26,7 @@ def main():
         color = '#ff3f3f'
         err_message = str(e)
 
-    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color, group_name=group_name)
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color, group_name=group_name, fail_url=fail_url, success_url=success_url)
 
 @app.route("/debug")
 def debug():
